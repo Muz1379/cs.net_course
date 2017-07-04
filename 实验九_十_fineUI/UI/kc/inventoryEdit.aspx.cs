5b2acb446f18c9c3d7df0ce7904493329b629c19
学生李令新 <stullx@qq.com>
2017-07-04 22:49:43 +0800

    库存盘点，主表修改

---
+++ 实验九_十_fineUI/UI/kc/inventoryEdit.aspx.cs
@@ -0,0 +1,88 @@
+﻿using BLL;
+using FineUI;
+using MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmptyProjectNet45_FineUI.kc
+{
+    public partial class inventoryEdit : System.Web.UI.Page
+    {
+        private static string keyNum;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                dp_billTime.MaxDate = DateTime.Now;
+                dp_invenDate.MaxDate = DateTime.Now;
+
+                //以下判断是否从正确路径访问页面
+                if (Request.UrlReferrer == null)
+                    Response.Redirect("~/login.aspx");
+
+                keyNum = Request["id"];
+
+                MODEL_kc_Inventory model = new MODEL.MODEL_kc_Inventory();
+                BLL_inventory bll = new BLL_inventory();
+
+                model = bll.GetModel("ID", keyNum);
+
+                dp_billTime.SelectedDate = model.BillDate;
+                dp_invenDate.SelectedDate = model.InventoryDate;
+
+                tbx_billNo.Text = model.InventoryNo;
+                tbx_factName.Text = model.FactoryName;
+                tbx_factNo.Text = model.FactoryNo;
+                dp_invenDate.SelectedDate = model.InventoryDate;
+                dp_billTime.SelectedDate = model.BillDate;
+                tbx_dutyName.Text = model.DutyName;
+                tbx_dutyNo.Text = model.DutyNo;
+                tbx_accName.Text = model.AssessorName;
+                tbx_accNo.Text = model.AssessorNo;
+                he_desc.Text = model.Description;
+                ddl_AssState.SelectedValue = model.AssState;
+            }
+        }
+
+        protected void btn_submit_Click(object sender, EventArgs e)
+        {
+            MODEL_kc_Inventory model = new MODEL.MODEL_kc_Inventory();
+            BLL_inventory bll = new BLL_inventory();
+            model.ID = keyNum;
+            model.InventoryNo = tbx_billNo.Text.ToString();
+            model.FactoryName = tbx_factName.Text.ToString();
+            model.FactoryNo = tbx_factNo.Text.ToString();
+            model.InventoryDate = dp_invenDate.SelectedDate.Value;
+            model.BillDate = dp_billTime.SelectedDate.Value;
+            model.DutyName = tbx_dutyName.Text.ToString();
+            model.DutyNo = tbx_dutyNo.Text.ToString();
+            model.AssessorName = tbx_accName.Text.ToString();
+            model.AssessorNo = tbx_accNo.Text.ToString();
+            model.Description = he_desc.Text.ToString();
+            model.AssState = ddl_AssState.SelectedValue.ToString();
+
+            if (bll.Update(model) > 0)
+            {
+                Alert alert = new Alert();
+                alert.Message = "修改成功";
+                alert.IconUrl = "~/res/images/success.png";
+                alert.Target = Target.Top;
+                alert.Show();
+                PageContext.RegisterStartupScript(ActiveWindow.GetHideRefreshReference());
+            }
+            else
+            {
+                Alert alert = new Alert();
+                alert.Message = "修改失败";
+                alert.IconUrl = "~/res/images/error_add.png";
+                alert.Target = Target.Top;
+                alert.Show();
+            }
+        }
+    }
+}
---
+++ 实验九_十_fineUI/UI/kc/inventoryEdit.aspx.designer.cs
@@ -0,0 +1,258 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能导致不正确的行为，如果
+//     重新生成代码，则所做更改将丢失。
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace EmptyProjectNet45_FineUI.kc {
+    
+    
+    public partial class inventoryEdit {
+        
+        /// <summary>
+        /// formMain 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formMain;
+        
+        /// <summary>
+        /// PageManager1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.PageManager PageManager1;
+        
+        /// <summary>
+        /// billNo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel billNo;
+        
+        /// <summary>
+        /// tbx_billNo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_billNo;
+        
+        /// <summary>
+        /// Panel10 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel10;
+        
+        /// <summary>
+        /// dp_invenDate 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.DatePicker dp_invenDate;
+        
+        /// <summary>
+        /// Panel2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel2;
+        
+        /// <summary>
+        /// tbx_factName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_factName;
+        
+        /// <summary>
+        /// Panel1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel1;
+        
+        /// <summary>
+        /// tbx_factNo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_factNo;
+        
+        /// <summary>
+        /// Panel4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel4;
+        
+        /// <summary>
+        /// dp_billTime 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.DatePicker dp_billTime;
+        
+        /// <summary>
+        /// Panel 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel;
+        
+        /// <summary>
+        /// lbl_acc 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Label lbl_acc;
+        
+        /// <summary>
+        /// ddl_AssState 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.DropDownList ddl_AssState;
+        
+        /// <summary>
+        /// Panel5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel5;
+        
+        /// <summary>
+        /// tbx_dutyName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_dutyName;
+        
+        /// <summary>
+        /// Panel6 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel6;
+        
+        /// <summary>
+        /// tbx_dutyNo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_dutyNo;
+        
+        /// <summary>
+        /// Panel7 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel7;
+        
+        /// <summary>
+        /// tbx_accName 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_accName;
+        
+        /// <summary>
+        /// Panel8 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel8;
+        
+        /// <summary>
+        /// tbx_accNo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.TextBox tbx_accNo;
+        
+        /// <summary>
+        /// Panel9 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel9;
+        
+        /// <summary>
+        /// he_desc 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.HtmlEditor he_desc;
+        
+        /// <summary>
+        /// Toolbar1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Toolbar Toolbar1;
+        
+        /// <summary>
+        /// btn_submit 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Button btn_submit;
+    }
+}
