788d70b7073915342fee84723ba32db39f38700d
学生李令新 <stullx@qq.com>
2017-07-04 22:47:32 +0800

    主子表类定义

---
+++ 实验九_十_fineUI/MODEL/MODEL_kc_Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODEL
+{
+    public class MODEL_kc_Inventory
+    {
+        public string ID { get; set; }
+        public string InventoryNo { get; set; }
+        public string FactoryNo { get; set; }
+        public string FactoryName { get; set; }
+        public DateTime InventoryDate { get; set; }
+        public DateTime BillDate { get; set; }
+        public string DutyNo { get; set; }
+        public string DutyName { get; set; }
+        public string AssessorNo { get; set; }
+        public string AssessorName { get; set; }
+        public string AssState { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ 实验九_十_fineUI/MODEL/MODEL_kc_InventorySub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODEL
+{
+    /// <summary>
+    /// 库存盘点子表
+    /// </summary>
+    public class MODEL_kc_InventorySub
+    {
+        public string ID { get; set; }
+        public string InventoryID { get; set; }
+        public string ProductNo { get; set; }
+        public string ProductName { get; set; }
+        public string Norm { get; set; }
+        public string storageNo { get; set; }
+        public string storageName { get; set; }
+        public int AccountAmount { get; set; }
+        public int InventoryAmount { get; set; }
+        public int ProfitLossAmount { get; set; }
+        public string Unit { get; set; }
+        public string Description { get; set; }
+    }
+}
